10b8d3b64da9783cb8479711faa3d1b8c5bd24d5
F. M Osman Faruq <155006600+fmosmanfaruq@users.noreply.github.com>
2025-03-09 22:52:23 +0600

    Add files via upload

---
+++ Problem-61A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,k;
+    cin>>s>>k;
+    for(int i=0; i<s.length(); i++){
+        if(s[i]==k[i]){
+            cout<<"0";
+        }
+        else{
+            cout<<"1";
+        }
+    }
+}
